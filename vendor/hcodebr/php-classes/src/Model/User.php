0aa1e1789e12125ed563ea807300eeb2271f1873
ronaldoemac <ronaldoemac@gmail.com>
2019-08-17 00:11:16 -0300

    Esqueceu a senha - Site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -192,7 +192,7 @@ class User extends Model {
 		));
 	}
 
-	public static function getForgot($email)
+	public static function getForgot($email, $inadmin = true)
 	{
 
 		$sql = new Sql();
@@ -234,7 +234,15 @@ class User extends Model {
 
 				$code = base64_encode($code);
 
-				$link = "http://www.roncodecommerce.com.br/admin/forgot/reset?code=$code";
+				if ($inadmin === true) {
+
+					$link = "http://www.roncodecommerce.com.br/admin/forgot/reset?code=$code";
+	
+				} else {
+
+					$link = "http://www.roncodecommerce.com.br/forgot/reset?code=$code";
+
+				}
 
 				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Desenv Ron Code", "forgot", array(
 					"name"=>$data["desperson"],
