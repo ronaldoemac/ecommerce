e245a48840e6d5a091ea87bc73f8ea23bd591479
ronaldoemac <ronaldoemac@gmail.com>
2019-09-04 19:38:10 -0300

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -11,6 +11,13 @@ function formatPrice($vlprice)
 	return number_format($vlprice, 2, ",", ".");
 }
 
+function formatDate($date)
+{
+
+	return date('d/m/Y', strtotime($date));
+	
+}
+
 function checkLogin($inadmin = true)
 {
 
